d50ca50561209786ddb403d9cf046081b3c51b9c
Carl Eriksson <carl.eriksson@funka.com>
2017-01-21 10:52:04 +0100

    setup azure

---
+++ Reflect/Azure/AzureContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Reflect.Azure
+{
+    public class AzureContext : DbContext
+    {
+
+        public AzureContext()
+           : base("name=azuredb")
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Question>().HasMany<Tag>(x => x.Tags);
+
+        }
+
+
+        //public virtual DbSet<Question> Questions { get; set; }
+        //public virtual DbSet<Tag> Tags { get; set; }
+
+
+    }
+}
