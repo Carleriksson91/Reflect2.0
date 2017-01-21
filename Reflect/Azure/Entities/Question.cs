4ff9cf3e32f597bace48ba62aa7b47c264d2a0a2
Carl Eriksson <carl.eriksson@funka.com>
2017-01-21 12:07:04 +0100

    nullable user in question

--- Reflect/Azure/Entities/Question.cs
+++ Reflect/Azure/Entities/Question.cs
@@ -5,17 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Reflect.Azure.Entities {
-   public class Question {
-      public int QuestionId { get; set; }
-      public string Title { get; set; }
-      public string Content { get; set; }
-      public Guid ApplicationUserId { get; set; }
-      public ApplicationUser ApplicationUser { get; set; }
-      public DateTime CreatedDate { get; set; }
-
-
-
-
-   }
+namespace Reflect.Azure.Entities
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid? ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
 }
