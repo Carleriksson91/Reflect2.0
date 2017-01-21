c3b6021a0027dd26e252b867e9862d03b9d5f693
ludvig skoglund <ludvig.skoglund@gmail.com>
2017-01-21 11:40:40 +0100

    Added entities Question and Tag

---
+++ Reflect/Azure/Entities/Question.cs
@@ -0,0 +1,21 @@
+﻿using Reflect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflect.Azure.Entities {
+   public class Question {
+      public int QuestionId { get; set; }
+      public string Title { get; set; }
+      public string Content { get; set; }
+      public Guid UserId { get; set; }
+      public ApplicationUser ApplicationUser { get; set; }
+      public DateTime CreatedDate { get; set; }
+
+
+
+
+   }
+}
