cf2960cff417586c02d76669cc6736ea671f0adb
Carl Eriksson <carl.eriksson@funka.com>
2017-01-21 10:35:26 +0100

    Add project files.

---
+++ Reflect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Reflect.MvcApplication" Language="C#" %>
---
+++ Reflect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Reflect.Startup))]
+namespace Reflect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
