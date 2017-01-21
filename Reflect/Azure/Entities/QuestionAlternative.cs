09da79e9a66a26ea1e934d654418ac7145ca237f
Carl Eriksson <carl.eriksson@funka.com>
2017-01-21 11:34:27 +0100

    added answer, and qa entities

---
+++ Reflect/Azure/Entities/QuestionAlternative.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reflect.Azure.Entities
+{
+    public class QuestionAlternative
+    {
+        public int QuestionAlternativeId { get; set; }
+
+        public string Title { get; set; }
+    }
+}
